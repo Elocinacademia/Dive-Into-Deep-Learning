1bee1b21552933a6f3c326a59bfa21a4c4913095
Elocinacademia <nicolezhanacademia@gmail.com>
2022-04-19 22:20:47 +0100

    Add overfit notes.

--- pytorch/my_practice.py
+++ pytorch/my_practice.py
@@ -4,11 +4,41 @@ import pandas as pd
 import csv
 # import pandas.util.testing
 import random
-import matplotlib
+# import matplotlib
 from d2l import torch as d2l
 
 
 # df = pd.util.testing.makeMixedDataFrame()
+
+
+from matplotlib import pyplot as plt
+
+plt.figure(figsize = (10,8))
+
+labels =[u'Quantitative', u'Qualitative', u'Theory', u'Mixed']
+sizes = [38,33,13,9]
+colors = ['#80AFBF','#608595','#DFC286','#C07A92']
+distance =0
+explode = (distance,distance,distance,distance)
+patches,text1,text2 = plt.pie(sizes,
+                             explode=explode,
+                             colors=colors,
+                              labels=None,
+                              labeldistance = 2.2,
+                              autopct = '%.2f%%',
+                              shadow = False,
+                              startangle = 90,
+                              pctdistance =0.8)
+plt.axis('equal')
+plt.title('Research Methods')
+plt.legend(labels,loc = 1,title = 'Method')
+plt.show()
+
+
+import pdb; pdb.set_trace()
+
+
+
 
 
 def synthetic_data(w, b, num_examples):
@@ -21,4 +51,3 @@ def synthetic_data(w, b, num_examples):
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
 features, labels = synthetic_data(true_w, true_b, 1000)
-import pdb; pdb.set_trace()
